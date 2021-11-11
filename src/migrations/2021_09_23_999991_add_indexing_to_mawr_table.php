00973613bf1b2a53d04d9a8c8dfaf17fd45812ed
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 12:03:39 +0530

    mawr

---
+++ src/migrations/2021_09_23_999991_add_indexing_to_mawr_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class addIndexingToMawr extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('fmt_mawr_ans')) {
+            Schema::table('fmt_mawr_ans', function (Blueprint $table) {
+                $table->index('question_id');
+                $table->index('active');
+                $table->index('arrange');
+                $table->index('media_id');
+            });
+        }
+
+        if (Schema::hasTable('fmt_mawr_ques')) {
+            Schema::table('fmt_mawr_ques', function (Blueprint $table) {
+                $table->index('active');
+                $table->index('media_id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('fmt_mawr_ans');
+    }
+}
